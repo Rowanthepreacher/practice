a4fae3f1a3c01b7b89e3384ca653d2b0c223f053
Rowanas <rowanthepreacher@hotmail.co.uk>
2023-11-29 13:50:25 +0000

    diceroller complete with explosion and non-explosion for any number of dice and attempts, with average

---
+++ dicecalc.py
@@ -0,0 +1,33 @@
+from random import randint
+
+def collateresults(dicetypes, times):
+    for dtype in dicetypes:
+        count = 1
+        rollaverageexp = []
+        rollaveragebump = []
+        while count <= times:
+            finalrollexp = rolldice(dtype)
+            finalrollbump = rollnoexplode(dtype)
+            rollaverageexp.append(finalrollexp)
+            rollaveragebump.append(finalrollbump)
+            count +=1
+        rollaverageexp = sum(rollaverageexp)/len(rollaverageexp)
+        rollaveragebump = sum(rollaveragebump)/len(rollaveragebump)
+        print(f"I rolled a d{dtype} {times} times and the average was {rollaverageexp}")
+        print(f"I rolled a d{dtype} {times} times and the average was {rollaveragebump}")
+        print(f"Over the course of {times} rolls of a d{dtype}, the difference between averages was {rollaverageexp - rollaveragebump}.")
+
+
+def rolldice(dtype):
+    rolloutput = randint(1,dtype)
+    if rolloutput == dtype:
+        rolloutput == rolloutput + rolldice(dtype)
+    return rolloutput
+
+def rollnoexplode(dtype):
+    noexplode = randint(1,dtype)
+    if noexplode <= 2:
+        noexplode == 3
+    return noexplode
+
+collateresults([4,6,8,10,12],10000)
